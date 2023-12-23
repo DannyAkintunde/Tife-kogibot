cb548da73d6acf5ca2155f577d70c874968df603
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2023-12-23 12:31:45 +0100

    Initial commit

---
+++ plugins/main-Guru.js
@@ -0,0 +1,25 @@
+let handler = async m => m.reply(`
+
+≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+https://chat.whatsapp.com/ItEGFz64Cz55HW6BNjxB2W
+
+
+
+─────────────
+≡ Disabled links? enter here! 
+
+▢ Group WhatsApp 
+ https://chat.whatsapp.com/ItEGFz64Cz55HW6BNjxB2W
+─────────────
+
+
+
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+
+export default handler
